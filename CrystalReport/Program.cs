8eee6ba2ffe50fef8903764b3f9e181c5b935cbc
jojobr <joaocopini11@gmail.com>
2023-11-14 10:53:28 -0300

    alteração de teste para o report

--- CrystalReport/Program.cs
+++ CrystalReport/Program.cs
@@ -1,6 +1,8 @@
-﻿using CrystalReport.Models;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalReport.Models;
 using System;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace CrystalReport
 {
@@ -28,9 +30,45 @@ namespace CrystalReport
                 ReportPath = args[8],
             };
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Report(data));
+            GenerateReport(data);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Report(data));
+        }
+
+        static void GenerateReport(ReportData _data)
+        {
+            // lembrar de mudar o Report para o servidor da Pasinato caso haja um problema de vendedor da base 
+            ReportDocument report = new ReportDocument();
+            // load do crystal passando o caminho da aplicação.
+            report.Load(_data.reportExeName);
+            report.Refresh();
+            report.DataSourceConnections.Clear();
+
+            // parâmetros do crystal. 
+            report.SetParameterValue("DateI", _data.periodo1);
+            report.SetParameterValue("DateF", _data.periodo2);
+            report.SetParameterValue("SlpCode", _data.cardCode);
+
+            for (int i = 0; i < report.Subreports.Count; i++)
+            {
+                report.SetParameterValue("DateI", _data.periodo1, report.Subreports[i].Name);
+                report.SetParameterValue("DateF", _data.periodo2, report.Subreports[i].Name);
+                report.SetParameterValue("SlpCode", _data.cardCode);
+            }
+
+            // dados de conexão OLE DB SQL
+            for (int index = 0; index < report.DataSourceConnections.Count; index++)
+            {
+                report.DataSourceConnections[index].SetConnection(_data.server, _data.banco, _data.user, _data.pass);
+            }
+            report.SetDatabaseLogon(_data.user, _data.pass);
+            // exporta para o PDF
+            string fileName = _data.ReportPath;
+
+            report.ExportToDisk(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, fileName);
+            report.Close();
+            report.Dispose();
         }
     }
 }
